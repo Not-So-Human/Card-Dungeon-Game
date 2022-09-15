29067c061695a618fa7825dab7dca3241e461f46
Lucifer8665 <76142200+Lucifer8665@users.noreply.github.com>
2022-09-15 10:25:47 -0400

    Revert "Animation"
    This reverts commit 6ca1c229abf4dc6fd26e57c554d100bf954ca7d8.

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -7,29 +7,15 @@ public class playerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
 
-    public float walkSpeed = 5f;
+    public float walkSpeed = 4f;
     public float speedLimiter = .7f;
     float inputHorizontal;
     float inputVertical;
-
-    //Animations and states
-    Animator animator;
-    string currentState;
-    const string PLAYER_IDLE = "Player_Idle";
-    const string PLAYER_WALK_LEFT_DOWN = "Player_WalkLeft_Down";
-    const string PLAYER_WALK_LEFT = "Player_WalkLeft";
-    const string PLAYER_WALK_LEFT_UP = "Player_WalkLeft_Up";
-    const string PLAYER_WALK_UP = "Player_WalkUp";
-    const string PLAYER_WALK_RIGHT_UP = "Player_WalkRight_Up";
-    const string PLAYER_WALK_RIGHT = "Player_WalkRight";
-    const string PLAYER_WALK_RIGHT_DOWN = "Player_WalkRight_Down";
-    const string PLAYER_WALK_DOWN = "Player_WalkDown";
-
     //this is to change the walk speed to the dash speed
     public float activeMoveSpeed;
     public float dashSpeed = 5f;
     //these are self explanitory
-    public float dashLength = .6f, dashCooldown = 3.5f;
+    public float dashLength = .7f, dashCooldown = 3.5f;
     //these are use to make it so you cant spam the dash
     public float dashCounter;
     public float dashCoolCounter;
@@ -38,7 +24,7 @@ public class playerMovement : MonoBehaviour
     {
         activeMoveSpeed = walkSpeed;
         rb = gameObject.GetComponent<Rigidbody2D>();
-        animator = gameObject.GetComponent<Animator>();
+       
     }
 
     // Update is called once per frame
@@ -69,7 +55,7 @@ public class playerMovement : MonoBehaviour
             }
         }
 
-        if (dashCoolCounter > 0)
+        if(dashCoolCounter > 0)
         {
             dashCoolCounter -= Time.deltaTime;
         }
@@ -79,7 +65,7 @@ public class playerMovement : MonoBehaviour
     {
         // Makes sure the speed you move diagonally is controlled
         if (inputHorizontal != 0 || inputVertical != 0)
-        {
+        { 
             if (inputHorizontal != 0 && inputVertical != 0)
             {
                 inputHorizontal *= speedLimiter;
@@ -87,61 +73,10 @@ public class playerMovement : MonoBehaviour
             }
 
             rb.velocity = new Vector2(inputHorizontal * activeMoveSpeed, inputVertical * activeMoveSpeed);
-           
-       
-            if (inputHorizontal > 0)
-            {
-                ChangeAnimationState(PLAYER_WALK_RIGHT);
-            }
-            else if (inputHorizontal > 0 && inputVertical > 0)
-            {
-                ChangeAnimationState(PLAYER_WALK_RIGHT_UP);
-            }
-            else if (inputHorizontal < 0 && inputVertical > 0)
-            {
-                ChangeAnimationState(PLAYER_WALK_LEFT_UP);
-            }
-            else if (inputHorizontal > 0 && inputVertical < 0)
-            {
-                ChangeAnimationState(PLAYER_WALK_RIGHT_DOWN);
-            }
-            else if (inputHorizontal < 0 && inputVertical < 0)
-            {
-                ChangeAnimationState(PLAYER_WALK_LEFT_DOWN);
-            }
-            else if (inputHorizontal < 0)
-            {
-                ChangeAnimationState(PLAYER_WALK_LEFT);
-            }
-            else if (inputVertical > 0)
-            {
-                ChangeAnimationState(PLAYER_WALK_UP);
-            }
-            else if (inputVertical < 0)
-            {
-                ChangeAnimationState(PLAYER_WALK_DOWN);
-            }
-            else if (inputVertical = 0 && inputHorizontal = 0)
-            // Make sures there is no input so it stops the character from moving
-            {
-
-                rb.velocity = new Vector2(0f, 0f);
-                ChangeAnimationState(PLAYER_IDLE);
-            }
-
-
         }
-        //animation state changer
-        void ChangeAnimationState(string newState)
-        {
-            //Stop animation from interrupting itself
-            if (currentState == newState) return;
-
-            //play new animation
-            animator.Play(newState);
-
-            //Update current state
-            currentState = newState;
-        }
+        else
+        // Make sures there is no input so it stops the character from moving
+            rb.velocity = new Vector2(0f, 0f);
     }
+    
 }
