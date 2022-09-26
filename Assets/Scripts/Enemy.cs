23f2c3d165ededee2efcf27ceb1ad9c0872517ac
Lucifer8665 <76142200+Lucifer8665@users.noreply.github.com>
2022-09-26 10:35:20 -0400

    Basic stuff
    Enemy fixed

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -10,9 +10,7 @@ public class Enemy : MonoBehaviour
 {
     Animator animator;
     string currentState;
-    const string ENEMY_LEFT = "Enemy_Left";
-    const string ENEMY_RIGHT = "Enemy_Right";
-    public float speed = 0.4f;
+    public float speed = 0.2f;
     public float Horizontal;
     Transform target;
 
@@ -22,10 +20,9 @@ public class Enemy : MonoBehaviour
         {
             float step = speed * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, target.position, step);
-            Horizontal = Input.GetAxisRaw("Horizontal");
         }
     }
-    
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
@@ -40,30 +37,5 @@ public class Enemy : MonoBehaviour
         {
             target = null;
         }
-    }
-    
-    void FixedUpdate()
-    {
-        if (Horizontal > 0)
-        {
-            ChangeAnimationState(ENEMY_RIGHT);
-        }
-        if (Horizontal < 0)
-        {
-            ChangeAnimationState(ENEMY_LEFT);
-        }
-    }
-    void ChangeAnimationState(string newState)
-    {
-        //Stop animation from interrupting itself
-        if (currentState == newState) return;
-
-        //play new animation
-        animator.Play(newState);
-
-
-
-        //Update current state
-        currentState = newState;
     }
 }
