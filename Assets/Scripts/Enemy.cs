abce1f81d9ad95a064017c940d0f06feb15b85ce
Lucifer8665 <76142200+Lucifer8665@users.noreply.github.com>
2022-09-26 07:57:01 -0400

    Enemy
    Its a basic enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,7 +8,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    Animator animator;
+    string currentState;
+    const string ENEMY_LEFT = "Enemy_Left";
+    const string ENEMY_RIGHT = "Enemy_Right";
     public float speed = 0.4f;
+    public float Horizontal;
     Transform target;
 
     void Update()
@@ -17,6 +22,7 @@ public class Enemy : MonoBehaviour
         {
             float step = speed * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, target.position, step);
+            Horizontal = Input.GetAxisRaw("Horizontal");
         }
     }
     
@@ -35,6 +41,29 @@ public class Enemy : MonoBehaviour
             target = null;
         }
     }
+    
+    void FixedUpdate()
+    {
+        if (Horizontal > 0)
+        {
+            ChangeAnimationState(ENEMY_RIGHT);
+        }
+        if (Horizontal < 0)
+        {
+            ChangeAnimationState(ENEMY_LEFT);
+        }
+    }
+    void ChangeAnimationState(string newState)
+    {
+        //Stop animation from interrupting itself
+        if (currentState == newState) return;
+
+        //play new animation
+        animator.Play(newState);
 
 
+
+        //Update current state
+        currentState = newState;
+    }
 }
